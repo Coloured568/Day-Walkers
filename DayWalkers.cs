3c3bbfcad396f17ad19ba24be8ce192d3ae1953a
_Coloured <79119198+Coloured568@users.noreply.github.com>
2024-08-01 20:36:07 -0400

    Update DayWalkers.cs
    added overlay gui

--- DayWalkers.cs
+++ DayWalkers.cs
@@ -8,10 +8,10 @@ namespace DayWalkers
     public class DayWalkersMod : MelonMod
     {
         private readonly float Dawn = 0.85f;
-        private readonly float Day = 1.15f;
+        private readonly float Day = 1.1f;
         private readonly float Night = 0.5f;
 
-        public float Time = 1.15f; // Default to Day      
+        public static float Time = 0.5f; // Default to Day      
 
         public override void OnInitializeMelon()
         {
@@ -30,6 +30,84 @@ namespace DayWalkers
             {
                 LoggerInstance.Msg("Skybox is not set in RenderSettings!");
             }
+        }
+
+        public static bool showMenu = false;
+        private static object selectedPlayer;
+        private static int selectedTab, yOffset = 0;
+        public static void ToggleMenu() => showMenu = !showMenu;
+        private static GUIStyle MainBox, Header, MainTabs = null;
+
+        public static float timeSlider = 0.5f;
+        private static float height;
+        private static float width;
+
+        private static Texture2D MakeTex(int width, int height, Color col)
+        {
+            Color[] pix = new Color[width * height]; for (int i = 0; i < pix.Length; ++i) pix[i] = col;
+
+            Texture2D result = new Texture2D(width, height);
+            result.SetPixels(pix);
+            result.Apply();
+            return result;
+        }
+        public static void DisplayHud()
+        {
+            var style = new GUIStyle();
+            GUI.BeginGroup(new Rect(10, 2, 340, 25));
+            Header = new GUIStyle(GUI.skin.box);
+            Header.normal.background = MakeTex(2, 2, new Color(1, 1, 1, 1));
+            Header.normal.textColor = Color.black;
+            GUI.Box(new Rect(10, 0, 328, 25), $"▼ Day-Walkers | V3.0.0 | Left Shift to open the overlay.", Header);
+            GUI.EndGroup();
+        }
+
+        public static void InitMenu()
+        {
+            var style = new GUIStyle();
+            style.normal.textColor = Color.white;
+
+            DisplayHud(); // Will show even if the menu isn't open	
+            if (!showMenu) return;
+            // Menu Background Setup
+            GUI.BeginGroup(new Rect(10, 10, 330, 500));
+            MainBox = new GUIStyle(GUI.skin.box);
+            MainBox.normal.background = MakeTex(2, 2, new Color(0.25f, 0.25f, 0.25f, 1.0f));
+            GUI.Box(new Rect(10, 12, 330, 500), $"https://patreon.com/nightrunnersgame", MainBox);
+            GUI.contentColor = Color.yellow;
+            GUI.Label(new Rect(50, 16.5f, 400, 42), $"___________________________________");
+            GUI.contentColor = Color.cyan;
+            MainTabs = new GUIStyle(GUI.skin.button);
+            MainTabs.normal.background = MakeTex(2, 2, new Color(0f, 0f, 1f, 0.75f));
+            MainTabs.normal.textColor = Color.black;
+            
+            Time = GUI.HorizontalSlider(new Rect(100, 40, 100, 30), Time, 0.5f, 1.1f);
+            GUI.Box(new Rect(100, 60, 100, 30), $"Exposure: {Time}");
+            GUI.Label(new Rect(25, 130, 300, 300), "Note: whenever you update the lightning, it does need a second to load!");
+            GUI.Label(new Rect(50, 180, 275, 200), "Day-walkers was devloped by: Coloured", style);
+
+            /* if (GUI.Button(new Rect(20, 40, 60, 27), (selectedTab == 0) ? "► Time" : "Main", MainTabs)) { selectedTab = 0; }
+
+            if (GUI.Button(new Rect(80, 40, 60, 27), (selectedTab == 1) ? "► Fun" : "Fun", MainTabs)) { selectedTab = 1; }
+            if (GUI.Button(new Rect(140, 40, 60, 27), (selectedTab == 2) ? "► World" : "World", MainTabs)) { selectedTab = 2; }
+            if (GUI.Button(new Rect(200, 40, 60, 27), (selectedTab == 3) ? "► Misc" : "Misc", MainTabs)) { selectedTab = 3; }
+            if (GUI.Button(new Rect(260, 40, 60, 27), (selectedTab == 4) ? "► Users" : "Users", MainTabs)) { selectedTab = 4; } */
+
+            // Choose the tab to render when selected.
+            /* switch (selectedTab)
+            {
+                case 0: break;
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+            } */
+            GUI.EndGroup();
+        }
+
+        public override void OnGUI()
+        {
+            InitMenu();
         }
 
         public override void OnUpdate()
@@ -65,6 +143,21 @@ namespace DayWalkers
                 Time = Day;
                 DisableBloomGlobally();
                 LoggerInstance.Msg("Set time to Day!");
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                if (Cursor.visible == false)
+                {
+                    Cursor.visible = true;
+                    Cursor.lockState = CursorLockMode.None;
+                    showMenu = true;
+                } else
+                {
+                    Cursor.visible = false;
+                    Cursor.lockState = CursorLockMode.Locked;
+                    showMenu = false;
+                }
             }
 
             // Continuously update the skybox exposure based on the Time variable
